5f42b4e2eea0ca19eb7aecbe86d2c5db0ac0cbf5
Peyton Moss <pmoss94@gmail.com>
2023-01-17 10:11:30 -0700

    vue basics

---
+++ vue_basics/app.js
@@ -0,0 +1,21 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      courseGoalA: "Learn Vue",
+      courseGoalB: "Master vue and build amazing apps",
+      vueLink: "https://vuejs.org/",
+    };
+  },
+  methods: {
+    outputGoal() {
+      const randomNumber = Math.random();
+      if (randomNumber < 0.5) {
+        return this.courseGoalA;
+      } else {
+        return this.courseGoalB;
+      }
+    },
+  },
+});
+
+app.mount("#user-goal");
